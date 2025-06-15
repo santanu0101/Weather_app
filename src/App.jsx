83b8d404d966cd08e175c27a9e0110c808197cf0
Santanu Raj <santanuraj937@gmail.com>
2025-06-15 20:32:10 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Weather from './assets/components/weather'
+import Weather from './components/weather'
 import { ToastContainer } from 'react-toastify';
 
 function App() {
